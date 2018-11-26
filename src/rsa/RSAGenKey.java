9217b5b0caa78c1d44ecc150bc37b9233fcd5040
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-11-26 11:16:12 -0800

    testing command line arguments

--- src/rsa/RSAGenKey.java
+++ src/rsa/RSAGenKey.java
@@ -3,8 +3,11 @@ package rsa;
 public class RSAGenKey {
     
     public static void main(String[] args) {
-        
+                       
         System.out.println("Hello world");
+        int count =  1;
+        for ( String arg : args )
+            System.out.println("This is command line argument # " + (count++) + " :  " + arg );
     
     }
         
