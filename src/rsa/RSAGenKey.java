2c0af474040672a6a9e3ca792c5d6b59fd194a92
jesus-r-mendoza <jesus_r_mendoza@yahoo.com>
2018-11-26 16:31:42 -0800

    checks for number of arguments

--- src/rsa/RSAGenKey.java
+++ src/rsa/RSAGenKey.java
@@ -1,14 +1,31 @@
 package rsa;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class RSAGenKey {
     
     public static void main(String[] args) {
-                       
-        System.out.println("Hello world");
-        int count =  1;
-        for ( String arg : args )
-            System.out.println("This is command line argument # " + (count++) + " :  " + arg );
+        
+        switch (args.length) {
+            case 1:
+                useSize(args[0]);
+                break;
+            case 3:
+                useGiven(args[0], args[1], args[2]);
+                break;
+            default:
+                System.out.println("\n*** ERROR : Given an invalid amount of arguments, nead 1 or 3 ; Given " + args.length + " ***");
+        }
+        
+    }
     
+    private static void useSize(String k) {
+        System.out.println(k);
+    }
+    
+    private static void useGiven(String p, String q, String e) {
+        System.out.println(p+", "+q+", "+e);
     }
         
 }
